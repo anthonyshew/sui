6516143733e4950956896f3004c24f2ab1f51736
github-action-benchmark <github@users.noreply.github.com>
2023-11-28 22:43:19 +0000

    add Benchmark (cargo) benchmark result for c4ddf994a9d3bc2f7ded57804dc33d3ce302154a

---
+++ bench/indexer/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1701211399136,
+  "repoUrl": "https://github.com/MystenLabs/sui",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "106119108+gegaowp@users.noreply.github.com",
+            "name": "Ge Gao",
+            "username": "gegaowp"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c4ddf994a9d3bc2f7ded57804dc33d3ce302154a",
+          "message": "Update indexer README.md (#14886)\n\n## Description \r\n\r\nmigration run -> database reset\r\nadd v2 and reader / writer instructions\r\n\r\n## Test Plan \r\n\r\neyeball\r\n\r\n---\r\nIf your changes are not user-facing and not a breaking change, you can\r\nskip the following section. Otherwise, please indicate what changed, and\r\nthen add to the Release Notes section as highlighted during the release\r\nprocess.\r\n\r\n### Type of Change (Check all that apply)\r\n\r\n- [ ] protocol change\r\n- [ ] user-visible impact\r\n- [ ] breaking change for a client SDKs\r\n- [ ] breaking change for FNs (FN binary must upgrade)\r\n- [ ] breaking change for validators or node operators (must upgrade\r\nbinaries)\r\n- [ ] breaking change for on-chain data layout\r\n- [ ] necessitate either a data wipe or data migration\r\n\r\n### Release notes",
+          "timestamp": "2023-11-28T14:35:57-08:00",
+          "tree_id": "bffa4e69fdc0211c7e76c3bcb7b91a76eb8b7a3b",
+          "url": "https://github.com/MystenLabs/sui/commit/c4ddf994a9d3bc2f7ded57804dc33d3ce302154a"
+        },
+        "date": 1701211394019,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "get_checkpoint",
+            "value": 393857,
+            "range": "± 23495",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
