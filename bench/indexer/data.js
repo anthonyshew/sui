9d9874023d735a474f3f1dfded6266daa3bceb0f
github-action-benchmark <github@users.noreply.github.com>
2023-11-29 00:34:20 +0000

    add Benchmark (cargo) benchmark result for 1b77f32a8c0c946cf7b2fdc26d49ce8e98b87474

--- bench/indexer/data.js
+++ bench/indexer/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701211399136,
+  "lastUpdate": 1701218060254,
   "repoUrl": "https://github.com/MystenLabs/sui",
   "entries": {
     "Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "get_checkpoint",
             "value": 393857,
             "range": "± 23495",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ashok@mystenlabs.com",
+            "name": "Ashok Menon",
+            "username": "amnn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1b77f32a8c0c946cf7b2fdc26d49ce8e98b87474",
+          "message": "[Tool] Package Dump (#15010)\n\n## Description\r\n\r\nTool to extract all move packages by talking directly to an instance of\r\nthe indexer database.\r\n\r\n## Test Plan\r\n\r\nManual:\r\n\r\n```\r\nsui$ cargo run --bin sui-pkg-dump -- --db-url <url> --output-dir <new-output-dir>\r\n```",
+          "timestamp": "2023-11-29T00:26:45Z",
+          "tree_id": "6139a956fb1db828acab805aed8b275d59c28b9c",
+          "url": "https://github.com/MystenLabs/sui/commit/1b77f32a8c0c946cf7b2fdc26d49ce8e98b87474"
+        },
+        "date": 1701218055605,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "get_checkpoint",
+            "value": 377622,
+            "range": "± 24287",
             "unit": "ns/iter"
           }
         ]
